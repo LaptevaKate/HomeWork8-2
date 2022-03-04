6178f36e11c1d51215a2b64746763456fdc922c9
LaptevaKate <ekaterina213@yandex.ru>
2022-03-04 14:04:01 +0300

    add code

---
+++ HomeWork8-2/StartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StartViewController.swift
+//  HomeWork8-2
+//
+//  Created by Екатерина Лаптева on 4.03.22.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- HomeWork8-2/ViewController.swift
+++ HomeWork8-2/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    // MARK: - Properties
+    
+    private let pannableView: UIView = {
+          
+        let view = UIView(frame: CGRect(origin: .zero,
+                                        size: CGSize(
+                                            width: 60,
+                                            height: 60)))
+        view.backgroundColor = .blue
+        view.layer.cornerRadius = 30
+        view.layer.masksToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    // MARK: - viewDidLoad
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(pannableView)
+        pannableView.center = view.center
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didPan(_:)))
+        pannableView.addGestureRecognizer(panGestureRecognizer)
+        }
+    
+    // MARK: - Actions
+    
+    @objc private func didPan(_ sender: UIPanGestureRecognizer) {
+        pannableView.center = sender.location(in: view)
     }
-
-
 }
 
